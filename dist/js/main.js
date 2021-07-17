b08d41f4fe488066d1f3db5d4614bc0a5c7977bf
Arseny Ivanov <arsenyivanov@MacBook-Pro-2.local>
2021-07-17 11:15:33 +0300

    update

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('This is a main.js');
+// Header nav effect
+var header = document.querySelector('.header');
+var headerHoverEffect = document.querySelector('.header__hover');
+
+console.log('header', header);
+
+header.onmousemove = function (e) {
+  headerHoverEffect.style.transform = 'translateX(' + (e.clientX - 50) + 'px';
+};
+
+header.onmouseenter = function () {
+  headerHoverEffect.classList.add('header__hover_active');
+};
+
+header.onmouseleave = function () {
+  headerHoverEffect.classList.remove('header__hover_active');
+};
--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log('This is a main.js')
+// Header nav effect
+const header = document.querySelector('.header')
+const headerHoverEffect = document.querySelector('.header__hover')
+
+console.log('header', header)
+
+header.onmousemove = e => {
+  headerHoverEffect.style.transform = `translateX(${e.clientX - 50}px`
+}
+
+header.onmouseenter = () => {
+  headerHoverEffect.classList.add('header__hover_active')
+}
+
+header.onmouseleave = () => {
+  headerHoverEffect.classList.remove('header__hover_active')
+}
